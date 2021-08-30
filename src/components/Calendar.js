be523757002415ced2bc50e0758b351db8e1cb36
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-08-30 22:44:31 +0200

    Adding function to handle click on date

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-export class App extends Component {
+export class Calendar extends Component {
+
+  handleDateClick = (arg) => {
+    alert(arg.dateStr)
+    console.log("Clicked on a date");
+  }
+
   render() {
     return (
       <div>
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-        weekends={true} // Show or hide weekends with true/false
-        events={[
-          { title: 'My birthday, yay!', date: '2021-08-28'},
-          { title: 'Second event', date: '2021-08-30'}
-        ]}
-      />
+        <FullCalendar
+          plugins={[ dayGridPlugin, interactionPlugin ]}
+          dateClick={this.handleDateClick}
+          initialView="dayGridMonth"
+          weekends={true} // Show or hide weekends with true/false
+          events={[
+            { title: 'My birthday, yay!', date: '2021-08-28'},
+            { title: 'Second event', date: '2021-08-30'}
+          ]}
+        />
       </div>
     )
   }
 }
 
-export default App;
+export default Calendar;
