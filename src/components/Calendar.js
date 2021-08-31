053aa53f78f5428c1dcbe75f4aff792e7a181ca4
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-08-31 21:46:40 +0200

    Moving stuff around and creating function to add new event (to calendar, not to json yet) clear

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,54 +1,97 @@
 import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
 
 export class Calendar extends Component {
 
+  calendarRef = React.createRef()
   state = {
     modal: false,
     currentEvents: []
   };
+  
+    // Fetch all events from json file and set as state
 
-  // Fetch all events from json file and set as state
-
-  componentDidMount() {
-    fetch('../events.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      this.setState({currentEvents: data})
+    componentDidMount() {
+      fetch('../events.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({currentEvents: data})
+      }
+      );
     }
-    );
-  }
-
-  // Callback function to handle what happens when clicking on a date
-
-  handleDateClick = (arg) => {
-    alert("Clicked on: " + arg.dateStr)
-    console.log("Clicked on a date");
-  }
-
-  handleEventClick = (info) => {
-    alert('Event: ' + info.event.title);
-    console.log("Clicked on event with title: " + info.event.title);
-  }
 
   render() {
     return (
       <div>
         <FullCalendar
-          plugins={[ dayGridPlugin, interactionPlugin ]}
-          dateClick={this.handleDateClick}
+          ref={this.calendarRef}
+          plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+          headerToolbar={{
+            left: 'prev next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          editable={true}
+          selectable={true}
           initialView="dayGridMonth"
-          weekends={true} // Show or hide weekends with true/false
           events={this.state.currentEvents}
           eventClick={this.handleEventClick}
+          select={this.handleDateSelect}
+          eventsSet={this.handleEvents}
+
+          // eventsDidUpdate={this.handleEventsDidUpdate}
+          // eventContent={renderEventContent}
+          // eventClick={this.handleEventClick}
+          // dateClick={this.handleDateClick}
         />
       </div>
     )
   }
+
+  // Function to create new event after click on date
+
+  handleDateSelect = (selectInfo) => {
+    let title = prompt('Add title for your event: ')
+    let calendarApi = this.calendarRef.current.getApi()
+
+    calendarApi.unselect()
+
+    if(title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: true
+      })
+      console.log("Added new event with title: " + title)
+    }
+  }
+
+
+  // handleEvents = (events) => {
+  //   this.setState({
+  //     currentEvents: events
+  //   })
+  // }
+
+
+  // Function to show info about selected event on click
+
+  handleEventClick = (info) => {
+    alert('Event: ' + info.event.title + ' Id:' + info.event.id);
+    console.log("Clicked on event with title: " + info.event.title + " and id: " + info.event.id);
+  }
+}
+
+let eventGuid = 0
+function createEventId() {
+  return String(eventGuid++)
 }
 
 export default Calendar;
